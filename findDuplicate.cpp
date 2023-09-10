38ac442230aa591aa2045b18565befd7dd502b9b
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-09-10 23:01:58 +0530

    findDuplicate.cpp

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -2,10 +2,10 @@ int findDuplicate(vector<int>&arr){
    int ans = 0;
 
    for(int i=0;i<n;i++){
-     ans = ans*arr[i];
+     ans = ans^arr[i];
    }
    for(int i=1;i<n;i++){
-     ans = ans*i;
+     ans = ans^i;
    }
   return ans;
 }
